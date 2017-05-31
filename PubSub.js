eaed140352a18b0e2148e8ef303742a18213c305
rantia <rushadantia@gmail.com>
2017-05-31 10:27:56 -0400

    D O C U M E N T A T I O N

--- PubSub.js
+++ PubSub.js
@@ -2,12 +2,18 @@
 'use strict';
 //TODO JSDOC add push button to affect all the other
 
-//holds sockets to each connected clients
-var connections = []
+
 var fs = require('fs');
 const hostname = require('os').hostname();
 
-//client class
+/**
+ * creates a client object which will make a connection to an endpoint
+ * @param {string} - ip ip address of server
+ * @param {int} port - port to connect to
+ * @param {function} dataHandler - a function that the client will send data to should be in format function(data){} where data is a {string}
+ * @constructor
+ * @class
+ */
 function Client(ip, port, dataHandler) {
     var n = require('net');
     this.client = new n.Socket();
@@ -18,16 +24,32 @@ function Client(ip, port, dataHandler) {
     this.dh = dataHandler;
 }
 
-//connects to endpoint and sends a number to it
+/**
+ * This function will start a connection to the endpoint given the
+ * ip and port that the client object was initialized with.
+ * It also logs all recieved data before sending it to the data handler
+ * if it was set by the user.
+ * @memberOf Client
+ */
 Client.prototype.run = function () {
     var that = this;
-    this.client.connect(this.port, this.ip, function () {
 
-    });
+    /**
+     * connects to the endpoint
+     * @memberOf Client.prototpye
+     * */
+    this.client.connect(this.port, this.ip);
 
-
+    /**
+     *  Callback when data is received
+     *  @memberOf Client.prototpye
+     * */
     this.client.on('data', function (data) {
+
+        //converts TCP stream data to a string
         const stringData = (new Buffer(data)).toString();
+
+        //splits the data at the :
         const dataArray = stringData.split(':');
 
         //if non formatted data or hostname data is received dont log it
@@ -40,38 +62,48 @@ Client.prototype.run = function () {
                 'data': dataArray[1]
             };
 
+            //push the data to out log
             that.log.push(JSON.stringify(logData));
+
+            //log the data to the screen
             console.log(logData);
 
+            //if the user defined a handler function send the string data to the function
             if (that.dh !== undefined)
                 that.dh(stringData);
         }
     });
-    /*function (data) {
 
-     if (data !== undefined) {
-
-     console.log('Received: ' + data);
-
-     }
-     });*/
-
-    //handles when the connection closes
+    /** Called when the socket has successfully closed
+     *  @memberOf Client.prototpye
+     **/
     this.client.on('close', function () {
         console.log(this.ip + ' closed')
     });
 
-    //handles errors
+    /**Called when an error like a refused connection, broken pipe, broken socket, etc has occured
+     * Notice when this is called the client class is broken
+     * @memberOf Client.prototpye
+     * */
     this.client.on('error', function () {
         console.log('error on' + this.ip);
     });
 
 };
-
+/**
+ * Will return the array contaning all of the logged data
+ * @returns {Array}
+ * @memberOf Client
+ */
 Client.prototype.getRXLog = function () {
     return this.log;
 };
 
+/**
+ * Deletes an entry from the log
+ * @param toRemove
+ * @memberOf Client
+ */
 Client.prototype.deleteFromLog = function (toRemove) {
     const i = this.log.indexOf(toRemove);
     if (i != -1) {
@@ -79,6 +111,12 @@ Client.prototype.deleteFromLog = function (toRemove) {
     }
 }
 
+/**
+ * Writes the entire log to a file specified by @param {filename} then clear the log
+ * Note this will append the data to that file
+ * @param filename
+ * @memberOf Client
+ */
 Client.prototype.writeLogToFile = function (filename) {
     var that = this;
     this.log.forEach(function (data) {
@@ -88,30 +126,45 @@ Client.prototype.writeLogToFile = function (filename) {
     });
 };
 
-//server class
-function Server(ip, port) {
+/**
+ * Creates a server instance. The server is used to accept multiple incoming connections and to send data to those connections
+ * @param {string} - ip ip address of server
+ * @param {int} port - port to connect to
+ * @param {int} timeout - The number of milliseconds of inactivity before a socket is presumed to have timed out. 0 will disable the timeout behavior
+ * @constructor
+ * @class
+ */
+function Server(ip, port,timeout) {
     this.ip = ip;
     this.port = port;
     this.server;
     this.seqnum = 0;
     this.log = []
+    this.timeout  = timeout
+    this.connections = []
 }
 
-//starts listening for connections
+/**
+ * Create a server at the specified ip and port and listen for connections
+ * @memberOf Server
+ */
 Server.prototype.start = function () {
     var net = require('net');
     var that = this;
 
+    /**
+     * Opens a server on the ip and port
+     */
     this.server = net.createServer(function (socket) {
 
-        //write server ip to client
-        socket.write(that.ip + '');
+        //write server hostname to client
+        socket.write(hostname + ':');
 
         //ignore random errors
         socket.on('error', function () {
         });
 
-        //get data
+        //get data if any is recieved from the client TODO allow client to request hostname and other data from server
         socket.on('data', function (data) {
             console.log((new Buffer(data)).toString());
         });
@@ -120,15 +173,19 @@ Server.prototype.start = function () {
         connections.push(socket);
     });
 
-
-    this.server.timeout = 0;
+    //
+    this.server.timeout = this.timeout;
 
     //listen for incoming connections
     this.server.listen(this.port, this.ip);
 
 };
 
-//sends data to connected clients
+/**
+ * Sends the data to all of the connected clients
+ * @param data {} - data to send to all the devices
+ * @memberOf Server
+ */
 Server.prototype.sendUpdate = function (data) {
     var that = this;
 
@@ -138,6 +195,7 @@ Server.prototype.sendUpdate = function (data) {
         //check if the socket is still alive
         if (value.address().address !== undefined) {
 
+            //start the tx timer
             const start = process.hrtime();
 
             //write data to end device
@@ -154,6 +212,7 @@ Server.prototype.sendUpdate = function (data) {
                 'tx event time': timetaken
             };
 
+            //print the data
             console.log(logData);
 
             //store that data in an array
@@ -161,7 +220,7 @@ Server.prototype.sendUpdate = function (data) {
         }
         //if its dead then remove it so we dont keep transmitting to a closed connection
         else {
-            const i = connections.indexOf(value);
+            const i = that.connections.indexOf(value);
             if (i != -1)
                 connections.splice(i, 1);
 
@@ -172,12 +231,20 @@ Server.prototype.sendUpdate = function (data) {
     this.seqnum++;
 };
 
-//gets the data log
+/**
+ * Returns the sent data log from the server
+ * @returns {Array}
+ * @memberOf Server
+ */
 Server.prototype.getTXLog = function () {
     return this.log;
 };
 
-//removes data element from the log
+/**
+ * Removes data element from the server log
+ * @param toRemove
+ * @memberOf Server
+ */
 Server.prototype.deleteFromLog = function (toRemove) {
     const i = this.log.indexOf(toRemove);
     if (i != -1) {
@@ -185,6 +252,11 @@ Server.prototype.deleteFromLog = function (toRemove) {
     }
 };
 
+/**
+ * Writes the server log data to a file
+ * @param filename
+ * @memberOf  Server
+ */
 Server.prototype.writeLogToFile = function (filename) {
     var that = this;
     this.log.forEach(function (data) {
@@ -194,12 +266,6 @@ Server.prototype.writeLogToFile = function (filename) {
     });
 };
 
-//returns list of connected devices
-function getNodeList() {
-    connections.forEach(function (sock) {
-        console.log(connections);
-    });
-}
 
 
 /****************************************************MAIN****************************************************/
