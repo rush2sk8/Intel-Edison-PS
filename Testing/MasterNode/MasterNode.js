c9441c30e9b6312473c49935da713d2191545f6b
rantia <rushadantia@gmail.com>
2017-06-01 15:14:11 -0400

    got the website semi-working

--- Testing/MasterNode/MasterNode.js
+++ Testing/MasterNode/MasterNode.js
@@ -1,35 +1,52 @@
-/*
- * Module dependencies
- */
-const port = 666
-var express = require('express')
-    , stylus = require('stylus')
-    , nib = require('nib')
+var sensors = [];
+sensors.push(['edison02','10.20.0.11']);
+sensors.push(['edison03', '10.20.0.12']);
+console.log(sensors);
+var express = require('express'),
+    app = express(),
+    http = require('http');
 
-
-var app = express()
-
-function compile(str, path) {
-    return stylus(str)
-        .set('filename', path)
-        .use(nib());
-}
-
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(express.logger('dev'))
-app.use(stylus.middleware(
-    { src: __dirname + '/public'
-        , compile: compile
-    }
-))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function (req, res) {
-    res.render('index',
-        { title : 'Home' }
-)
-})
+    var html = '<!DOCTYPE html>' +
+        ' <html>' +
+        ' <head>' +
+        '<title>Edison Control Center</title>' +
+        '<link rel="stylesheet" href="style.css">' +
+        '    </head>' +
+        '  <body>' +
+        '   <header>' +
+        ' <h1>Edison Control Center</h1>' +
+        '</header>' +
+        ' <div class="container">' +
+        '    <div class="main-content">' +
 
-app.listen(port)
-console.log('server running at localhost:'+port)
+        '  <div id="table">' +
+        '<table cellpadding="14"cellspacing="14">';
+    html += '<tr><th>Sensor ID</th><th>Sensor IP</th></tr>';
+
+    sensors.forEach(function (s) {
+
+        html += '<tr>';
+        html += '<th>' + s[0] + '</th>';
+        html += '<th>' + s[1] + '</th>';
+        html += '</tr>';
+    });
+
+    html += '</table>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '<footer>' +
+        '<p>© 2017 Rushad Antia</p>' +
+        '</footer>' +
+        '</body>' +
+        ' </html>';
+
+    res.send(html);
+});
+
+app.listen(3000, function () {
+    console.log('Website running at localhost:3000');
+});
