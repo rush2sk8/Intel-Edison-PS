40252c7ffb284232a542583f57b429ebc6487fe4
Rushad Antia <rushadantia@gmail.com>
2017-06-13 15:48:01 -0400

    added batch file to generate documentation

--- PubSub/client.js
+++ PubSub/client.js
@@ -30,7 +30,6 @@ function Client(ip, port, dataHandler) {
     this.port = port;
     this.log = []
     this.dh = dataHandler;
-    //   this.mnc = mnc;
 }
 
 /** 
@@ -79,7 +78,7 @@ Client.prototype.run = function () {
                 'data': dataArray[1]
             };
 
-       
+
             //push the data to out log 
             that.log.push(JSON.stringify(logData));
 
@@ -106,10 +105,6 @@ Client.prototype.run = function () {
     this.client.on('close', function () {
         console.log(that.ip + ' closed');
         that.client.destroy();
-
-        //uncomment if you want the master node to know when a connection is dropped
-        // if (that.mnc !== undefined)
-        //    that.mnc.write('cld-' + that.connHN + '-' + that.ip);
 
     });
 
---
+++ generate documentation.bat
@@ -0,0 +1,3 @@
+cd C:\Users\sensor\Documents\Node.jsTesting\Intel-Edison-PS ;
+jsdoc PubSub\client.js PubSub\MasterNodeConnection.js PubSub\PubSub.js PubSub\server.js MasterNode\MasterNode.js
+pause
