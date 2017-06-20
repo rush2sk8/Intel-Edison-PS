38749e94a78c555b8b66dbe11af68cc929f16b31
Rushad Antia <rushadantia@gmail.com>
2017-06-20 14:25:45 -0400

    no more udp

--- PubSub/MasterNodeConnection.js
+++ PubSub/MasterNodeConnection.js
@@ -1,7 +1,7 @@
 var Client = require('./client.js');
 var Server = require('./server.js');
 var net = require('net');
-var clientConnToMN;
+
 /**
 *
 * @param ip -- ip address of the master node
@@ -12,7 +12,6 @@ var clientConnToMN;
 * @constructor
 * @example
 * var master = new MasterNodeConnection('10.20.0.128', 9999, 'light:', '', dh);
-* master.startAutomaticDiscovery();
 */
 function MasterNodeConnection(ip, port, mysensors, want, dh) {
   this.ip = ip;
@@ -27,143 +26,103 @@ function MasterNodeConnection(ip, port, mysensors, want, dh) {
 
 /**
 * Starts the discovery and connection service
+* @example
+* var master = new MasterNodeConnection('10.20.0.128', 9999, 'light:', '', dh);
+* master.startAutomaticDiscovery();
 */
 MasterNodeConnection.prototype.startAutomaticDiscovery = function () {
   const that = this;
   this.server = new Server(this.myIP, 1337, 0);
 
+  //start the server on the device
   this.server.start();
 
   //master connection
-  clientConnToMN = new net.Socket();
+  var clientConnToMN = new net.Socket();
 
-  /*
-  //connect to master node endpoint
-  clientConnToMN.connect(this.port, this.ip, function () {
-  this.write('nn-' + (require('os').hostname()) + '-' + that.myIP + '-' + that.mySensors + '-' + that.want);
-});
+  //function to connect to the Master Node
+  var startClientConn = function() {
 
-//handle the actual pub/sub creation
-clientConnToMN.on('data', function (data) {
+    //create a socket connection
+    clientConnToMN = new net.Socket();
 
-//put data into a string
-const stringData = (new Buffer(data)).toString();
-
-stringData.split('*').forEach(function (node) {
-
-//split the command
-var command = node.split('-');
-
-//if the command is a new node in the network
-if (command[0] === 'ct') {
-
-//create a new client
-var newClient = new Client(command[2], 1337, that.dh);
-
-// the client connection
-newClient.run();
-
-//keep a track of ongoing connections
-that.clients.push(newClient);
-
-console.log('connected to: ' + command[1]);
-} else if (command[0] == 'ping') {
-
-//ping in 2017
-clientConnToMN.write('pong');
-}
-
-});
-});
-
-//TODO add automatic reconnect
-clientConnToMN.on('close', function () {
-
-clientConnToMN.destroy();
-clientConnToMN = new net.Socket();
-
-clientConnToMN.connect(that.port, that.ip, function () {
-this.write('nn-' + (require('os').hostname()) + '-' + that.myIP + '-' + that.mySensors + '-' + that.want);
-console.log('reconnected to mnc');
-});
-
-clientConnToMN.on('error', function () {
-console.log('mnc error')
-});
-
-});
-*/
-
-var startClientConn = function() {
-  clientConnToMN = new net.Socket();
-
-  clientConnToMN.connect(that.port, that.ip, function () {
-    this.write('nn-' + (require('os').hostname()) + '-' + that.myIP + '-' + that.mySensors + '-' + that.want);
-    console.log('connected to MN')
-  });
-
-  //handle the actual pub/sub creation
-  clientConnToMN.on('data', function (data) {
-
-    //put data into a string
-    const stringData = (new Buffer(data)).toString();
-
-    stringData.split('*').forEach(function (node) {
-
-      //split the command
-      var command = node.split('-');
-
-      //if the command is a new node in the network
-      if (command[0] === 'ct') {
-
-        //create a new client
-        var newClient = new Client(command[2], 1337, that.dh);
-
-        // the client connection
-        newClient.run();
-
-        //keep a track of ongoing connections
-        that.clients.push(newClient);
-
-        console.log('connected to: ' + command[1]);
-      }
-
+    //make the connection
+    clientConnToMN.connect(that.port, that.ip, function () {
+      this.write('nn-' + (require('os').hostname()) + '-' + that.myIP + '-' + that.mySensors + '-' + that.want);
+      console.log('Connected to MN')
     });
+
+    //handle the actual pub/sub creation
+    clientConnToMN.on('data', function (data) {
+
+      //put data into a string
+      const stringData = (new Buffer(data)).toString();
+
+      stringData.split('*').forEach(function (node) {
+
+        //split the command
+        var command = node.split('-');
+
+        //if the command is a new node in the network
+        if (command[0] === 'ct') {
+
+          //create a new client
+          var newClient = new Client(command[2], 1337, that.dh);
+
+          // the client connection
+          newClient.run();
+
+          //keep a track of ongoing connections
+          that.clients.push(newClient);
+
+          console.log('connected to: ' + command[1]);
+        }
+
+      });
+    });
+
+    //ignore errors
+    clientConnToMN.on('error', function() { });
+
+    //if error is thrown this will be called and will handle a reconnect to the MN
+    clientConnToMN.on('close', function () {
+      clientConnToMN.destroy();
+      console.log('Connection to MN closed');
+      setTimeout(startClientConn(), 1000);
+      console.log('retrying...')
+    });
+
+  };
+
+  //start the connection
+  startClientConn();
+
+  //handle exits
+  process.on('SIGINT', function () {
+
+    //write the tx data to a log file
+    that.server.writeLogToFile('(' + (new Date()) + ') tx.log');
+
+    //write all the rx logs to a file
+    that.clients.forEach(function (c) {
+      c.writeLogToFile(c.ip + ' (' + (new Date()) + ') rx.log');
+    });
+
+    //tell the MN that we have closed
+    clientConnToMN.write('cld-' + require('os').hostname() + '-' + that.ip);
+
+    console.log('\nwrote data to log files');
+
+    //wait b4 exiting
+    setTimeout(function () {
+      process.exit();
+    }, 1000);
+
   });
 
-  clientConnToMN.on('error', function() {
-    console.log('Connection to MN error')
+  clientConnToMN.on('error', function () {
+    console.log('mnc error')
   });
-
-  clientConnToMN.on('close', function () {
-    console.log('connection to MN closed');
-    setTimeout(startClientConn(), 5000);
-  });
-
-};
-
-startClientConn();
-
-process.on('SIGINT', function () {
-  that.server.writeLogToFile('(' + (new Date()) + ') tx.log');
-
-  that.clients.forEach(function (c) {
-    c.writeLogToFile(c.ip + ' (' + (new Date()) + ') rx.log');
-  });
-
-  clientConnToMN.write('cld-' + require('os').hostname() + '-' + that.ip);
-
-  console.log('\nwrote data to log files');
-
-  setTimeout(function () {
-    process.exit();
-  }, 1000);
-
-});
-
-clientConnToMN.on('error', function () {
-  console.log('mnc error')
-});
 
 };
 
