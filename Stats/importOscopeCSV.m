7191ba667d4290392a9c2649a01e36aa73c4832d
Rushad Antia <rushadantia@gmail.com>
2017-07-13 13:08:03 -0400

    stats

--- Stats/importOscopeCSV.m
+++ Stats/importOscopeCSV.m
@@ -11,7 +11,7 @@ function Trace0 = importOscopeCSV(filename)
 
 %% Initialize variables.
 if nargin < 1
-    filename = 'C:\Users\sensor\Documents\Keysight\BenchVue\Oscilloscope\Exports\Trace 0.csv';
+    filename = 'C:\Users\sensor\Documents\Node.jsTesting\Intel-Edison-PS\Stats\Trace 0.csv';
 end
 delimiter = ',';
 startRow = 2;
